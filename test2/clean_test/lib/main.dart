ed36eb95caa464be8786f7eb8f8c2800ffcd53e3
kirimikh2008 <kirimikh2008@gmail.com>
2026-04-03 19:49:39 +0300

    1

--- test2/clean_test/lib/main.dart
+++ test2/clean_test/lib/main.dart
@@ -14,11 +14,31 @@ void main() {
               flexibleSpace: FlexibleSpaceBar(
                 centerTitle: true,
                 title: const Text(
-                  'Курсы валют',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  'Exchange rates',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    // color: Colors.white,
+                  ),
                 ),
               ),
               expandedHeight: 200,
+            ),
+
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(100.0),
+                    child: Title(
+                      color: Colors.white,
+                      child: Text('Currency converter'),
+                    ),
+                  ),
+
+                  Padding(padding: EdgeInsets.all(12.0), child: TextField()),
+                ],
+              ),
             ),
           ],
         ),
